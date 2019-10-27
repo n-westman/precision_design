fa66cf306a5a6ae61cdc45fa4349c79f3da49ae5
n-westman <niklas@Niklass-MacBook-Pro.local>
2019-10-27 21:54:31 +0100

    Updates

---
+++ precache-manifest.52cfc0e849c6c0599e77366bf2759cad.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "9ac776c253b72fba4668b5d5dea389ed",
+    "url": "/precision_design/static/media/unserious-guy.9ac776c2.png"
+  },
+  {
+    "revision": "c4869f1e97704279629fe9054929a2b8",
+    "url": "/precision_design/static/media/serious-guy.c4869f1e.png"
+  },
+  {
+    "revision": "55a65ba4482302265360",
+    "url": "/precision_design/static/js/runtime~main.a819639e.js"
+  },
+  {
+    "revision": "cd9c8b438bc8e3875cb4",
+    "url": "/precision_design/static/js/main.77a0ac0a.chunk.js"
+  },
+  {
+    "revision": "fe7f6a3a917bb59b0aa5",
+    "url": "/precision_design/static/js/2.57ad4bc6.chunk.js"
+  },
+  {
+    "revision": "cd9c8b438bc8e3875cb4",
+    "url": "/precision_design/static/css/main.a2d0e66b.chunk.css"
+  },
+  {
+    "revision": "391ebb5c955144a1ac08c034e3975add",
+    "url": "/precision_design/index.html"
+  }
+];
